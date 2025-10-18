126b089de1230507fee3006c5b7134d3e24bca54
ricodev38 <eric.vinsonnaud@gmail.com>
2025-10-18 16:30:16 +0200

    Update NonBlockingDelay.h

--- src/NonBlockingDelay.h
+++ src/NonBlockingDelay.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <Arduino.h>
+
+class NonBlockingDelay {
+protected:
+	unsigned long previousTime;
+	unsigned long delay;
+
+public:
+	NonBlockingDelay(int aDelay = 0);
+    bool TimeReached();
+	void SetDelay(unsigned long aDelay);
+    void Reset();
+};
 
